bed02df0a293a48f6020a6be5c6fa891cb0318e8
usmanmobeen111 <usmanmobeen111@gmail.com>
2025-08-25 23:36:11 +0500

    First Commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import TaskManager from './pages/TaskManager'
+
+
+createRoot(document.getElementById('root')).render(
+
+    <TaskManager/>
+
+)
---
+++ src/pages/TaskManager.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TaskManager = () => {
+  return (
+    <div className=''>
+      Task Manager
+    </div>
+  )
+}
+
+export default TaskManager
